d3eecfb8647e038ac576c0ef5a4c73bade8def30
OlivierPalma <olivierpalmasouza@gmail.com>
2017-05-08 19:00:54 -0300

    Criando a classe usuarioe implementando o metodo loadByID

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
   require_once 'config.php';
 
+  /*
   $sql = new Sql();
 
   $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
   echo json_encode($usuarios);
+  */
+
+  $usuario = new Usuario();
+
+  $usuario->loadByID(1);
+
+  echo $usuario;
 
 ?>
